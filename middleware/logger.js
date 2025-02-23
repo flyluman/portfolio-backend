ce52940c48051dd1407d8c4ef49d38693d078afc
Luman Rahman <flyluman@gmail.com>
2025-02-23 08:23:13 +0600

    fix ip

--- middleware/logger.js
+++ middleware/logger.js
@@ -7,7 +7,7 @@ export const QUERY_CACHE = {};
 export const logger = async (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
 
-    const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
+    const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip.replace(/^::ffff:/, '');
 
     if (!IP_CACHE[ip]) {
         const geoData = await fetchGeoData(ip);
